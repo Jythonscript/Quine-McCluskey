7accd25125e822441d321c5ced0de8fd83862430
eric4190 <eric4190@umn.edu>
2020-02-11 22:08:49 -0600

    Added a combine function in data and labeled some data members with purposes

--- data.py
+++ data.py
@@ -3,16 +3,23 @@ binLength = int(input("Number of bits: "))
 
 # data structure
 class Data:
+    # Inherent Data Types:
+    #num The number this binary represents
+    #bin the binary representation of this number
+    #parents = [] the parents of this binary representation, or the nubers it came from
+
 
     # default constructor
     def __init__(self, numStr):
         try:
             self.num = int(numStr)
             self.bin = format(int(numStr), '0' + str(binLength) + 'b')
+            self.parents = [self.num]
         except ValueError:
             self.num = -1
             self.bin = numStr
-        self.parents = []
+            self.parents = []
+
 
     # return the number of "1" digits in the binary representation of the number
     def numOnes(self):
@@ -54,3 +61,15 @@ def canCombine(data1, data2):
         return True
     else:
         return False
+
+def combine(data1, data2):
+    """This function will need to take two objects as a member and return another object that is a result of the two combined"""
+    for i in range(len(data1.bin)):
+        if data1.bin[i] != data2.bin[i]:
+            # looks for a difference, since these can combine you know there is only one difference in the two strings
+            new_string = data1.bin
+            new_string = new_string[:i] + '-' + new_string[i+1:]
+            new_data = Data(new_string)
+    new_data.parents = data1.parents + data2.parents
+    return new_data
+
